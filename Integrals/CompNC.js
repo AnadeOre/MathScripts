756bf4c565c5fada69af6a49b28509df7e3ce8b7
Ana <anaemilia.deorellana@gmail.com>
2020-11-17 01:36:39 -0300

    Composite NC added

--- Integrals/CompNC.js
+++ Integrals/CompNC.js
@@ -9,9 +9,10 @@ const fstr = prompt('Please enter the function f(x): ')
 const f = math.parse(fstr);
 const a = parseFloat(prompt('Enter the first number of the interval: '));
 const b = parseFloat(prompt('Enter the last number of the interval: '));
+const L = parseFloat(prompt('Enter the number of subintervals: '));
 const n = parseFloat(prompt('Enter the amount of points for the inverval: '));
 
-integralNC(f, a, b, n)
+CompNC(f, a, b, L, n)
 
 function weights(n) {
     //Calculates weights of the Newton Côtes formula for n points
@@ -21,6 +22,7 @@ function weights(n) {
     for (i = n; i >= 0; i--) {
         x[i] = (i * 1 + (n - i) * 0) / n;
     } //x = linspace(0, 1, n);
+
 
     A = new Array(n + 1).fill(1);
 
@@ -48,24 +50,39 @@ function weights(n) {
     return math.lusolve(Am, bm)
 }
 
-function integralNC(f, a, b, n) {
-    //Calculates the integral of f over [a,b] using the Newton-Côtes formula for n points
+function CompNC(f, a, b, L, n) {
+    //Calculates the integral of f over [a,b] using the Composed Newton-Côtes formula for n points
 
     w = weights(n);
     var i, y = Array(n);
-    n2 = n - 1;
-    for (i = n2; i >= 0; i--) {
-        y[i] = (i * b + (n2 - i) * a) / n2;
+    for (i = L; i >= 0; i--) {
+        y[i] = (i * b + (L - i) * a) / L;
     }
-    fy = new Array(n).fill(1)
-    for (var j = 0; j < n; j++) {
-        let eval = {
-            x: y[j],
-        }
-        fy[j] = f.evaluate(eval);
-    }
+    h = math.divide((b - a), L);
+    let Q = 0;
+    let fz = new Array(n).fill(1)
 
-    const fvec = math.matrix(fy)
-    Q = math.multiply((b - a), (math.multiply(fvec, w).subset(math.index(0))));
+    for (var l = 0; l < L; l++) {
+
+        let z = linspace(y[l], y[l + 1], n);
+
+        for (var k = 0; k < n; k++) {
+            let eval = {
+                x: z[k],
+            }
+            fz[k] = f.evaluate(eval);
+        }
+        const fvec = math.matrix(fz)
+        Q = Q + math.multiply(h, (math.multiply(fvec, w).subset(math.index(0))));
+    }
     console.log('The integral of ', fstr, ' between ', a, ' and ', b, ' is: ', Q)
+}
+
+function linspace(a, b, n) {
+    if (typeof n === "undefined") n = Math.max(Math.round(b - a) + 1, 1);
+    if (n < 2) { return n === 1 ? [a] : []; }
+    var i, ret = Array(n);
+    n--;
+    for (i = n; i >= 0; i--) { ret[i] = (i * b + (n - i) * a) / n; }
+    return ret;
 }
